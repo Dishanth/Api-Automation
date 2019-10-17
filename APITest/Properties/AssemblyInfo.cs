c77d09c608bfbc63decd58639a332e1a8c1d3247
Dishanth <ndishanthn@gmail.com>
2019-10-17 10:52:02 -0400

    Add files via upload

---
+++ APITest/Properties/AssemblyInfo.cs
@@ -0,0 +1,20 @@
+using System.Reflection;
+using System.Runtime.CompilerServices;
+using System.Runtime.InteropServices;
+
+[assembly: AssemblyTitle("APITest")]
+[assembly: AssemblyDescription("")]
+[assembly: AssemblyConfiguration("")]
+[assembly: AssemblyCompany("")]
+[assembly: AssemblyProduct("APITest")]
+[assembly: AssemblyCopyright("Copyright ©  2019")]
+[assembly: AssemblyTrademark("")]
+[assembly: AssemblyCulture("")]
+
+[assembly: ComVisible(false)]
+
+[assembly: Guid("707eeca5-dbae-4704-b7a6-c8e4f544e2b3")]
+
+// [assembly: AssemblyVersion("1.0.*")]
+[assembly: AssemblyVersion("1.0.0.0")]
+[assembly: AssemblyFileVersion("1.0.0.0")]
---
+++ APITest/UnitTest1.cs
@@ -0,0 +1,75 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Threading.Tasks;
+using System.Web.UI;
+using APITest.Modal;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+using RestSharp;
+using RestSharp.Serialization.Json;
+
+namespace APITest
+{
+    [TestClass]
+    public class UnitTest1
+    {
+        //Step1 : Start the fake server
+        //https://github.com/typicode/json-server#getting-started
+
+        [TestMethod]
+        public void GetOperation()
+        {
+            var client = new RestClient("http://localhost:3000/");
+            var request = new RestRequest("posts/{postId}", Method.GET);
+            request.AddUrlSegment("postId", 3);
+            var response = client.Execute(request);
+
+            var deserialize = new JsonDeserializer();
+            var output= deserialize.Deserialize<Dictionary<string,string>>(response);           
+            Assert.AreEqual(output["id"], "3", "Error");
+        }
+
+        [TestMethod]
+        public void PostOperation()
+        {
+            var client = new RestClient("http://localhost:3000/");
+            var request = new RestRequest("posts", Method.POST);          
+            request.AddJsonBody(new Post() {author="First", title="first 1" });
+
+            //var response = client.Execute(request);
+            //var deserialize = new JsonDeserializer();
+            //var output = deserialize.Deserialize<Dictionary<string, string>>(response);
+            //Assert.AreEqual(output["author"], "First", "Error");
+
+            //or
+
+            var response = client.Execute<Post>(request).Data;          //Generic way
+            Assert.AreEqual(response.author, "First", "Error");
+        }
+
+        [TestMethod]
+        public void PostAsyncOperation()
+        {
+            var client = new RestClient("http://localhost:3000/");
+            var request = new RestRequest("posts", Method.POST);
+            request.AddJsonBody(new Post() { author = "First", title = "first 1" });
+            
+            var response = client.Execute<Post>(request).Data;          //Generic way
+            Assert.AreEqual(response.author, "First", "Error");
+        }
+
+        private async Task<IRestResponse<T>> ExecuteAsyncRequest<T>(RestClient client , IRestRequest request)
+        {
+            var taskCompletionSource = new TaskCompletionSource<IRestResponse<T>>();
+            client.ExecuteAsync<T>(request, restresponse =>
+            {
+                if (restresponse.ErrorException != null)
+                {
+                    throw new ApplicationException("Error");
+                }
+                taskCompletionSource.SetResult(restresponse);
+            });
+            return await taskCompletionSource.Task;
+        }
+
+    }
+}
